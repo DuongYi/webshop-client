4195dcd022be9f3e7a56bddfa32eb3e4be34c6bd
DuongYi <duonguet1903@gmail.com>
2021-10-30 20:43:24 +0700

    add product Vieww

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -25,100 +25,103 @@ import HomeBanner from "./components/HomeBanner";
 import NewsSlick from "./components/NewsSlick";
 
 const useStyles = makeStyles(() => ({
-  // home_about: {
-  // },
-  // home_product: {
-  // },
-  // arrow: {
-  //   color: "#000"
-  // },
-  // home_shop_container: {
-  //   position: 'relative',
-  //   '&:before': {
-  //     content: "''",
-  //     position: 'absolute',
-  //     top: 27,
-  //     left: 0,
-  //     width: '100%',
-  //     height: '100%',
-  //     backgroundImage: `url(${'static/images/collections-bg.png'})`,
-  //     backgroundSize: 'cover',
-  //     backgroundRepeat: 'no-repeat',
-  //     backgroundPosition: 'center',
-  //     minHeight: '640px',
-  //     paddingBottom: '30px',
-  //     zIndex: -1
-  //   }
-  // },
-  // allShop_button: {
-  //   display: 'inline-flex',
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   minWidth: '280px',
-  //   height: '50px',
-  //   borderRadius: '25px',
-  //   border: '2px solid #fff',
-  //   backgroundColor: 'hsla(0,0%,100%,.6)',
-  //   padding: '0 50px',
-  //   color: '#fff',
-  //   fontSize: '16px',
-  //   fontWeight: 500,
-  //   textDecoration: 'none',
-  //   transition: 'all ease 0.3s',
-  //   '&:hover': {
-  //     backgroundColor: '#738136',
-  //   }
-  // },
-  // home_resources: {
-  //   marginTop: '74px',
-  //   overflow: 'hidden',
-  //   paddingBottom: '20px'
-  // },
-  // home_resources_header: {
-  //   flex: '0 0 600px',
-  //   maxWidth: '600px',
-  // },
-  // home_resources_btn: {
-  //   display: 'inline-flex',
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   borderRadius: '5px',
-  //   backgroundColor: '#e7edd7',
-  //   padding: '0 30px',
-  //   fontSize: '14px',
-  //   fontWeight: 500,
-  //   lineHeight: 1.21,
-  //   letterSpacing: '2.8px',
-  //   color: '#738136',
-  //   height: '45px',
-  //   border: 0,
-  //   transition: 'all 0.3s',
-  //   '&:hover': {
-  //     color: '#fff',
-  //     backgroundColor: '#738136'
-  //   }
-  // },
-  // home_react: {
-  // },
-  // box_react: {
-  //   padding: '80px 0 165px',
-  //   backgroundImage: `url(${'static/images/mewspapers.png'})`,
-  //   backgroundSize: 'cover',
-  //   backgroundRepeat: 'no-repeat',
-  //   backgroundPosition: 'center',
-  //   borderRadius: '15px',
-  // },
-  // homeNews_thumbnail: {
-  //   textAlign: 'center',
-  //   width: '100%',
-  //   marginTop: '-100px',
-  // },
-  // home_blog: {
-  //   width: '100%',
-  //   padding: '0 15px',
-  //   display: 'flex',
-  //   direction: 'column'
-  // }
+  root: {
+    color: '#000'
+  },
+  home_about: {
+  },
+  home_product: {
+  },
+  arrow: {
+    color: "#000"
+  },
+  home_shop_container: {
+    position: 'relative',
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      top: 27,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundImage: `url(${'static/images/collections-bg.png'})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '640px',
+      paddingBottom: '30px',
+      zIndex: -1
+    }
+  },
+  allShop_button: {
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: '280px',
+    height: '50px',
+    borderRadius: '25px',
+    border: '2px solid #fff',
+    backgroundColor: 'hsla(0,0%,100%,.6)',
+    padding: '0 50px',
+    color: '#fff',
+    fontSize: '16px',
+    fontWeight: 500,
+    textDecoration: 'none',
+    transition: 'all ease 0.3s',
+    '&:hover': {
+      backgroundColor: '#738136',
+    }
+  },
+  home_resources: {
+    marginTop: '74px',
+    overflow: 'hidden',
+    paddingBottom: '20px'
+  },
+  home_resources_header: {
+    flex: '0 0 600px',
+    maxWidth: '600px',
+  },
+  home_resources_btn: {
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px',
+    backgroundColor: '#e7edd7',
+    padding: '0 30px',
+    fontSize: '14px',
+    fontWeight: 500,
+    lineHeight: 1.21,
+    letterSpacing: '2.8px',
+    color: '#738136',
+    height: '45px',
+    border: 0,
+    transition: 'all 0.3s',
+    '&:hover': {
+      color: '#fff',
+      backgroundColor: '#738136'
+    }
+  },
+  home_react: {
+  },
+  box_react: {
+    padding: '80px 0 165px',
+    backgroundImage: `url(${'static/images/mewspapers.png'})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    borderRadius: '15px',
+  },
+  homeNews_thumbnail: {
+    textAlign: 'center',
+    width: '100%',
+    marginTop: '-100px',
+  },
+  home_blog: {
+    width: '100%',
+    padding: '0 15px',
+    display: 'flex',
+    direction: 'column'
+  }
 }));
 
 function Home() {
@@ -194,7 +197,7 @@ function Home() {
   return (
     <Page>
       <HomeBanner />
-      {/* <Container maxWidth="lg" style={{ marginTop: '50px' }}>
+      <Container maxWidth="lg" style={{ marginTop: '50px' }}>
         <Grid
           container
           spacing={12}
@@ -207,8 +210,7 @@ function Home() {
             <Typography
               variant="h2"
               style={{
-                margin: '0 0 5px', color: '#4c503d',
-                fontSize: '42px', fontFamily: " Yeseva One, cursive", fontWeight: 'bold'
+                margin: '0 0 5px', color: '#4c503d', fontSize: '42px', fontFamily: " Yeseva One, cursive", fontWeight: 'bold'
               }}
             >
               Chuyện của cỏ
@@ -261,6 +263,7 @@ function Home() {
                     marginBottom: '25px', // 4
                   }
                 }}
+                className={classes.root}
               >
                 {
                 items.map((item) => <AboutItem key={item} item={item} />)
@@ -281,8 +284,7 @@ function Home() {
             <Typography
               variant="h2"
               style={{
-                margin: '0 0 15px', color: '#4c503d',
-                fontSize: '36px', fontFamily: " Yeseva One, cursive", fontWeight: 'bold'
+                margin: '0 0 15px', color: '#4c503d', fontSize: '36px', fontFamily: " Yeseva One, cursive", fontWeight: 'bold'
               }}
             >
               Sản phẩm bán chạy nhất
@@ -303,8 +305,7 @@ function Home() {
             <Typography
               variant="h2"
               style={{
-                color: '#4c503d', fontSize: '36px', fontFamily: " Yeseva One, cursive",
-                padding: '40px 0 0', marginRight: '-50px'
+                color: '#4c503d', fontSize: '36px', fontFamily: " Yeseva One, cursive", padding: '40px 0 0', marginRight: '-50px'
               }}
             >
               Gian hàng
@@ -315,17 +316,16 @@ function Home() {
             <Typography
               variant="h2"
               style={{
-                color: '#738136', fontSize: '36px',
-                fontFamily: " Yeseva One, cursive", padding: '40px 0 0', marginLeft: '-45px'
+                color: '#738136', fontSize: '36px', fontFamily: " Yeseva One, cursive", padding: '40px 0 0', marginLeft: '-45px'
               }}
             >
               Cỏ mềm
             </Typography>
           </Grid>
         </Box>
-      </Container> */}
+      </Container>
 
-      {/* <Container maxWidth="" className={classes.home_shop_container}>
+      <Container maxWidth="" className={classes.home_shop_container}>
         <Container maxWidth="lg" style={{ zIndex: 1 }}>
           <Tabs
             style={{
@@ -377,7 +377,7 @@ function Home() {
           </Box>
         </Container>
         <Box />
-      </Container> */}
+      </Container>
 
       {/* <Container maxWidth="lg" className={classes.home_resources}>
         <Grid
@@ -450,7 +450,7 @@ function Home() {
         </div>
       </Container> */}
 
-      {/* <Container maxWidth="lg" style={{ marginTop: '60px' }}>
+      <Container maxWidth="lg" style={{ marginTop: '60px' }}>
         <TrackSlick />
       </Container>
 
@@ -467,8 +467,7 @@ function Home() {
             variant="h2"
             mb={2}
             style={{
-              color: '#4c503d', fontSize: '36px',
-              fontFamily: " Yeseva One, cursive", padding: '40px 0 0'
+              color: '#4c503d', fontSize: '36px', fontFamily: " Yeseva One, cursive", padding: '40px 0 0'
             }}
           >
             Blog làm đẹp
@@ -484,7 +483,7 @@ function Home() {
           </Typography>
         </Grid>
         <BlogSlick />
-      </Container> */}
+      </Container>
       <Footer />
     </Page>
   );
--- src/views/ProductView/index.js
+++ src/views/ProductView/index.js
@@ -1,15 +1,12 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core';
 import {
-  Box,
-  Container,
   Grid,
-  Typography
-} from '@mui/material';
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 
-import CarouselSlick from "src/components/CarouselSlick";
 import Footer from 'src/components/Footer';
 import Page from 'src/components/Page';
 
@@ -21,80 +18,35 @@ const useStyles = makeStyles(() => ({
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     borderRadius: '10px',
+  },
+  pRelated_title: {
+    fontFamily: 'Yeseva One, sans-serif',
+    textAlign: 'center',
+    fontWeight: 400,
+    fontSize: '24px',
+    lineHeight: '28px',
+    color: '#fff',
+    marginBottom: '45px'
   }
 }));
 
 function ProductView() {
   const classes = useStyles();
 
-  const productList = [
-    {
-      title: "Son kem Nhung (Lì mượt không silicon)",
-      image: "static/images/sp1.jpg",
-      price: "150.000đ"
-    },
-    {
-      title: "Sữa tắm hương HOA SỮA Hà Nội và Em (Giới hạn 2000 chai duy nhất)",
-      image: "static/images/sp2.jpg",
-      price: "150.000đ"
-    },
-    {
-      title: "Nước hoa Unisex Dreamer",
-      image: "static/images/sp3.jpg",
-      price: "350.000đ"
-    },
-    {
-      title: "Kem Chống Nắng Hây Hây",
-      image: "static/images/sp4.jpg",
-      price: "260.000đ"
-    },
-    {
-      title: "Dầu gội thảo dược Tóc Mây",
-      image: "static/images/sp5.jpg",
-      price: "325.000đ"
-    },
-    {
-      title: "Son Lụa không chì",
-      image: "static/images/sp6.jpg",
-      price: "180.000đ"
-    },
-    {
-      title: "Bột khử mùi Wings Up - Khô thoáng tuyệt đối",
-      image: "static/images/sp7.jpg",
-      price: "115.000đ"
-    },
-    {
-      title: "Serum dưỡng da ban đêm Hoàng Lan",
-      image: "static/images/sp8.jpg",
-      price: "180.000đ"
-    },
-  ];
-
   return (
     <Page>
-      <Container maxWidth="lg">
-        {/* <Box className={classes.product_related}>
-          <Grid
-            container
-            display="flex"
-            justifyContent="center"
-          >
-            <Typography
-              variant="h2"
-              style={{
-                margin: '0 0 60px', color: '#fff',
-                fontSize: '24px', fontFamily: " Yeseva One, cursive"
-              }}
-            >
-              Sản phẩm liên quan
-            </Typography>
-          </Grid>
-          <Box style={{ padding: '0 20px 0 40px' }}>
-            <CarouselSlick items={productList} />
-          </Box>
-
-        </Box> */}
-        <h1>Hello</h1>
+      <Container>
+        <Grid
+          container
+          display="flex"
+          direction="column"
+          alignItems="center"
+          className={classes.product_related}
+        >
+          <Typography className={classes.pRelated_title}>
+            Sản phẩm liên quan
+          </Typography>
+        </Grid>
       </Container>
       <Footer />
     </Page>
